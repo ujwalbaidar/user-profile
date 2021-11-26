d779fb3f4ff3013e21d9082afae19401353440d0
ujwal <ujwal.baidar@student.griffith.ie>
2021-11-26 00:34:08 +0000

    display username, firstname, loading message

--- src/app/profile-settings/profile-settings.component.ts
+++ src/app/profile-settings/profile-settings.component.ts
@@ -9,16 +9,33 @@ export class ProfileSettingsComponent implements OnInit {
   public title = 'Profile';
   public user: IProfile|undefined;
   
+  public displayLoad = false;
+  public displaySave = false;
+  public displayError = false;
 
-  
   constructor(private profile: ProfileService) { }
-
+  /** 
+   * call getUserProfile on initialization 
+   * while data is not received display loading as true
+   **/
   ngOnInit(): void {
+    this.displayLoad = true;
     this.getuserProfile();
   }
-
+  /** 
+   * get user profile from profileService 
+   * when data is fetched hide "display loading" message 
+   * if error occurs reload the method "getuserProfile()"
+   **/
   getuserProfile(){
- 
+    this.profile.getProfileUser()
+      .then(userdata=>{
+        this.displayLoad = false;
+        this.user = userdata;
+      })
+      .catch(err=>{
+        this.getuserProfile();
+      })
 
   }
   saveProfile(){}
